c989b61f24e976a89b69953acdbbaea7ff12ec40
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-09-20 17:33:59 -0300

    Added React Router

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
